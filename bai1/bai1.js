618c9dad2e7299afc29e97bd5e10e03cf2baefd8
tongquockhanh2510 <22677951.khanh@student.iuh.edu.vn>
2025-01-08 19:33:17 +0700

    hoan thanh bai 1

--- bai1/bai1.js
+++ bai1/bai1.js
@@ -22,22 +22,22 @@ let BMIJohn2 = tinhBMI(weightJohn2, heightJohn2);
 let markHigherBMI2 = BMIMark2 >BMIJohn2;
 
 //Dữ liệu 11
-console.log('Bộ dữ liệu test 1'); 
+console.log('Test 1'); 
 console.log('BMI của Mark:',BMIMark.toFixed(2));
 console.log('BMI của Joh:',BMIJohn.toFixed(2));
 if(markHigherBMI){
-    console.log('BMI của Mark hơn John'); 
+    console.log('Marks BMI is higher than John'); 
 } else{
-    console.log('BMI của John hơn MarkMark'); 
+    console.log('Johns BMI is higher than Marks!'); 
 }
 
 //bộ dữ liệu test 2
-console.log('Bộ dữ liệu test 2'); 
-console.log('BMI của Mark:',BMIMark2.toFixed(2));
-console.log('BMI của Joh:',BMIJohn2.toFixed(2));
+console.log('Test 2'); 
+console.log('Mark BMI:',BMIMark2.toFixed(2));
+console.log('Join BMI:',BMIJohn2.toFixed(2));
 if(markHigherBMI2){
-    console.log('BMI của Mark hơn John'); 
+    console.log('Marks BMI is higher than John'); 
 } else{
-    console.log('BMI của John hơn MarkMark'); 
+    console.log('Johns BMI is higher than Marks!'); 
 }
 
